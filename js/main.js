927fbacf1965daa47a6b3e90d8895fb26deb1fd0
lyamoore <lapshovamasha@mail.ru>
2023-09-07 21:23:12 +0300

    search bar

--- js/main.js
+++ js/main.js
@@ -5,28 +5,51 @@ const $blockWindInfo = document.querySelector(".footer__info__wind");
 const $blockPressureInfo = document.querySelector(".footer__info__pressure");
 const $blockHumidityInfo = document.querySelector(".footer__info__humidity");
 
-const $blockChange = document.querySelector(".header__change");
-const $blockChangeTitle = document.querySelector(".header__change__title");
+// const $blockChange = document.querySelector(".header__change");
+// const $blockChangeTitle = document.querySelector(".header__change__title");
 const $blockChangeSearch = document.querySelector(".header__change__search");
+const $blockChangeInput = document.querySelector(
+  ".header__change__search__input"
+);
+const $blockChangeBtn = document.querySelector(".header__change__search__send");
 
 let city = "London";
 
+// async function getWeather(city) {
+//   let response = await fetch(
+//     `http://api.weatherapi.com/v1/current.json?key=5cf81997e51e495cb8184339230509&q=${city}&aqi=no`
+//   );
+
+//   let jsonRespone = await response.json();
+
+//   writeInfo(jsonRespone);
+// }
+
 async function getWeather(city) {
+  let data = await getData(city);
+
+  writeInfo(data);
+}
+
+async function getData(city) {
   let response = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=5cf81997e51e495cb8184339230509&q=${city}&aqi=no`
   );
 
   let jsonRespone = await response.json();
+  return jsonRespone;
+}
 
-  writeMainInfo(jsonRespone);
-  writeAdditInfo(jsonRespone);
+function writeInfo(data) {
+  writeMainInfo(data);
+  writeAdditInfo(data);
 }
 
 function writeMainInfo(data) {
   const temperature =
     data.current.temp_c > 0
-      ? `${city} +${data.current.temp_c}`
-      : `${city} ${data.current.temp_c}`;
+      ? `${data.location.name} +${data.current.temp_c}`
+      : `${data.location.name} ${data.current.temp_c}`;
 
   const condition = data.current.condition.text;
 
@@ -46,17 +69,49 @@ function writeAdditInfo(data) {
   $blockHumidityInfo.insertAdjacentHTML("beforeend", humidity);
 }
 
-setInterval(getWeather(city), 30 * 60 * 1000);
+setInterval(getWeather(city), 10 * 60 * 1000);
 
-function changeOn() {
-  $blockChangeTitle.style.display = "none";
-  $blockChangeSearch.style.display = "block";
+// СМЕНА ГОРОДА
+
+$blockChangeBtn.addEventListener("click", searchCityClick);
+$blockChangeInput.addEventListener("keyup", searchCityBtn);
+
+function searchCityClick() {
+  let newCity = $blockChangeInput.value;
+
+  clearInfo();
+  getWeather(newCity);
+  $blockChangeInput.value = "";
 }
 
-function changeOver() {
-  $blockChangeTitle.style.display = "block";
-  $blockChangeSearch.style.display = "none";
+function searchCityBtn(e) {
+  if (e.key == "Enter") {
+    let newCity = e.target.value;
+
+    clearInfo();
+    getWeather(newCity);
+    e.target.value = "";
+  }
 }
 
-$blockChangeTitle.addEventListener("mouseover", changeOn);
-$blockChangeTitle.addEventListener("mouseout", changeOver);
+function clearInfo() {
+  $blockMainInfo.innerHTML = "";
+  $blockWindInfo.innerHTML = "";
+  $blockPressureInfo.innerHTML = "";
+  $blockHumidityInfo.innerHTML = "";
+}
+
+// ИСЧЕЗНОВЕНИЕ ЧЕНДЖ СИТИ
+
+// function changeOn() {
+//   $blockChangeTitle.style.display = "none";
+//   $blockChangeSearch.style.display = "block";
+// }
+
+// function changeOver() {
+//   $blockChangeTitle.style.display = "block";
+//   $blockChangeSearch.style.display = "none";
+// }
+
+// $blockChangeTitle.addEventListener("mouseover", changeOn);
+// $blockChangeTitle.addEventListener("mouseout", changeOver);
