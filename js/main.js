aeb41f948d1fe75e6091239a6d8022ee6c75f369
lyamoore <lapshovamasha@mail.ru>
2023-09-11 22:22:38 +0300

    interval tuning

--- js/main.js
+++ js/main.js
@@ -1,19 +1,44 @@
 "use strict";
 
+let city = "London";
+let intervalId;
+
 const $blockMainInfo = document.querySelector(".main__info");
-const $blockWindInfo = document.querySelector(".footer__info__wind");
-const $blockPressureInfo = document.querySelector(".footer__info__pressure");
-const $blockHumidityInfo = document.querySelector(".footer__info__humidity");
+const $blockWindInfo = document.querySelector(".footer__info__field__wind");
+const $blockPressureInfo = document.querySelector(
+  ".footer__info__field__pressure"
+);
+const $blockHumidityInfo = document.querySelector(
+  ".footer__info__field__humidity"
+);
+
+// СТРОКА ПОИСКА
 
 const $blockChangeSearch = document.querySelector(".header__change__search");
 const $blockChangeInput = document.querySelector(
   ".header__change__search__input"
 );
 const $blockChangeBtn = document.querySelector(".header__change__search__send");
+const $blockChangeWarn = document.querySelector(
+  ".header__change__search__warn"
+);
 
-let city = "London";
+// МЕНЮ
+
+const $blockMenu = document.querySelector(".footer__menu");
+
+// ОБНОВЛЕНИЕ ДАННЫХ И ПЕРЕРИСОВКА
+
+function updateAndReloadData(city) {
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+
+  intervalId = setInterval(getWeather(city), 10 * 60 * 1000);
+}
 
 async function getWeather(city) {
+  clearInfo();
   const data = await getData(city);
 
   writeInfo(data);
@@ -57,7 +82,12 @@ function writeAdditInfo(data) {
   $blockHumidityInfo.insertAdjacentHTML("beforeend", humidity);
 }
 
-setInterval(getWeather(city), 10 * 60 * 1000);
+function clearInfo() {
+  $blockMainInfo.innerHTML = "";
+  $blockWindInfo.innerHTML = "";
+  $blockPressureInfo.innerHTML = "";
+  $blockHumidityInfo.innerHTML = "";
+}
 
 // СМЕНА ГОРОДА
 
@@ -68,11 +98,12 @@ async function searchCityClick() {
   const newCity = $blockChangeInput.value;
 
   if (await checkCity(newCity)) {
-    clearInfo();
-    getWeather(newCity);
+    updateAndReloadData(newCity);
 
     $blockChangeInput.value = "";
   } else {
+    warningCity();
+
     $blockChangeInput.value = "";
   }
 }
@@ -82,11 +113,12 @@ async function searchCityBtn(e) {
     const newCity = e.target.value;
 
     if (await checkCity(newCity)) {
-      clearInfo();
-      getWeather(newCity);
+      updateAndReloadData(newCity);
 
       e.target.value = "";
     } else {
+      warningCity();
+
       e.target.value = "";
     }
   }
@@ -100,22 +132,37 @@ async function checkCity(city) {
   return response.ok ? 1 : 0;
 }
 
-function clearInfo() {
-  $blockMainInfo.innerHTML = "";
-  $blockWindInfo.innerHTML = "";
-  $blockPressureInfo.innerHTML = "";
-  $blockHumidityInfo.innerHTML = "";
+function warningCity() {
+  $blockChangeWarn.style.opacity = 1;
+  setTimeout(() => ($blockChangeWarn.style.opacity = 0), 3 * 1000);
 }
 
-// ЗАПРЕЩАЮ ПИСАТЬ НА РУССКОМ
+// ТОЛЬКО АНГЛИЙСКИЙ В СТРОКЕ ПОИСКА
 
 $blockChangeInput.addEventListener("input", washField);
 
 function washField(e) {
   const value = e.target.value;
-  const regex = /^[A-Za-z]+$/;
+  const regex = /^[A-Za-z\s]+$/;
 
   if (!regex.test(value)) {
-    e.target.value = value.replace(/[^A-Za-z]/g, "");
+    e.target.value = value.replace(/[^A-Za-z\s]/g, "");
   }
 }
+
+// ВЗАИМОДЕЙСТВИЕ С МЕНЮ
+
+$blockMenu.addEventListener("click", switchMenu);
+
+function switchMenu(e) {
+  const id = e.target.textContent;
+  const elem = document.querySelector(`#${id}`);
+
+  elem.style.display = "none";
+
+  console.log(e.target.textContent);
+}
+
+// ЗАПУСК
+
+updateAndReloadData(city);
